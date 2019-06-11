f2c543fbc1771d960cd46c4e3130690878b8a9bd
XhantiGhost <xhantijacob@gmail.com>
2019-06-11 18:24:14 +0200

    what

---
+++ script.js
@@ -0,0 +1,68 @@
+//initialize these three variables
+var a = 5;
+var b = 10;
+var c = "i am a string";
+
+//Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + "string!";
+
+// js maths
+
+var add = 3 + 5;
+var sub = 5 - 4;
+var multi = 8 * 7;
+var divide = 66 / 33;
+
+//incriment values
+var myVar = 87;
+//only change code below this line
+
+myVar++;
+
+//decrimennt values
+var myVar = 87;
+myVar--;
+
+//find the remainder of values;
+var remainder;
+remainder = 11 % 3;
+
+//Compound assignment with augmented addition
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+//only modify code below this line'
+
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+//we can instead do this
+
+a += 12;
+b += 9;
+c += 7;
+
+//same with subtraction
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+//same with multiplication and division
+
+a *= 11;
+c /= 2;
+
+//strings
+
+myStr = "I am a" +  "your mama"
+
+
+ 
+ 
